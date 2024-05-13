91cb9f3beb1b56aa0e56261918c796c0714529cb
Felixumansky <85160369+Felixumansky@users.noreply.github.com>
2024-05-13 14:57:37 +0300

    cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import "dotenv/config"; // import env
 const port = process.env.PORT || 3000;
-
+import cors from "cors";
 var app = express();
 import otpRouter from "./routes/opt.router.js";
+app.use(cors()); // Use this after the variable declaration
+
 app.use(express.json());
 app.all(
 	/^\/\.?env/i,
